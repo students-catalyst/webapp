6bd80a6db9beaa4374b620a9084bec2b3de6f455
root <mahdiarnaufal@gmail.com>
2018-08-03 14:15:18 +0700

    homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bioskop Kampus Itb' });
 });
 
 module.exports = router;
