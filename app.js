169929274870f46b200650f37dc0de6998f641df
root <mahdiarnaufal@gmail.com>
2018-08-03 14:34:41 +0700

    add login page

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var homepage = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', homepage);
+app.use('/login', login);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ routes/login.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Bioskop Kampus Itb' });
+});
+
+module.exports = router;
