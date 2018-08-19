d8ed4a0891c10df38a0e6c0e17534e68a787b45d
root <mahdiarnaufal@gmail.com>
2018-08-19 19:44:52 +0700

    change start script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 
-let port = (process.env.PORT || 3001);
+let port = (process.env.PORT || 8080);
 server.listen(port);
 console.log(`Server listens on port ${port}`);
 
